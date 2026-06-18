6f5137faf3fa4a56d658d98364795c659b6b422c
aouirora <auroraruci12@gmail.com>
2026-06-18 20:49:25 +0200

    Exercise 3: raise error if not 6 fields

--- mapper.py
+++ mapper.py
@@ -3,5 +3,9 @@ import sys
 
 for line in sys.stdin:
     data = line.strip().split("\t")
+
+    if len(data) != 6:
+        raise ValueError("Expected 6 fields, got {0}".format(len(data)))
+
     date, time, item, category, sales, payment = data
     sys.stdout.write("{0}\t{1}\n".format(category, sales))
